a09808c28cc439310068096eb5ef0a3ba7fbacd3
cutemiya <k-shaipov@mail.ru>
2023-12-16 12:10:44 +0300

    хз

--- repository/migrations/migrations.py
+++ repository/migrations/migrations.py
@@ -1,25 +1,16 @@
-events = '''
-create table if not exists event (
-    id serial primary key,
-    title text not null,
-    description text not null,
-    company_id int references public.CompanyDetails(id)
-);
-'''
-
 user_tags = '''
 create table if not exists usertag (
     id serial primary key,
     tag text not null,
-    account_id int references account(id)
+    account_id int references "Account"(id)
 );
 '''
 
 user_events = '''
 CREATE TABLE IF NOT EXISTS user_events (
     id serial primary key,
-    event_id int references event(id),
-    account_id int references account(id),
+    event_id int references "Event"(id),
+    account_id int references "Account"(id),
     place int default 0,
     status int default 0 -- 0 - идет, 1 - прошло
 );
